3d4477a91846b1be0e8becdf2095dcbec7b9b8a9
miljonhunden <jian0415@gmail.com>
2015-03-10 14:47:10 +0100

    popup form

--- js/view/PlanningViewController.js
+++ js/view/PlanningViewController.js
@@ -1,10 +1,29 @@
 var PlanningViewController = function(view, model ) {
 	
-			
+	//Function To Display Popup		
 	$("#add-activity").click(function(){
+		$("#popupAddActivity").show();
+			
+
+/*			alert("activity box");	
 		
-		alert("activity box");
-	});
+		
+		
+		function check_empty() {
+			if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+				alert("Fill All Fields !");
+			} 
+
+			else {
+				document.getElementById('form').submit();
+					alert("Form Submitted Successfully...");
+			}
+			}
+
+	*/});
+		$("#confirm-activity").click(function(){
+			$("#popupAddActivity").hide();
+		});
 
 
 
