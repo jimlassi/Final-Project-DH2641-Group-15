1f1c1addbd49e2c2b5ee6f8bb3ecb793fd85abfe
tompa119 <anderssontomase@gmail.com>
2015-03-10 16:24:56 +0100

    New examples activities

--- js/view/StartViewController.js
+++ js/view/StartViewController.js
@@ -8,14 +8,12 @@ var StartViewController = function(view, model) {
 		
 		
 		model.addDay();
+		//create parked activities
+		model.addActivity(new Activity("Introduction",10,0,"Introduction for this day"));
+		model.addActivity(new Activity("Breaktime biznezz",20,0,"Boboddy"));
 
-		model.addActivity(new Activity("Introduction",10,0,""),0);
-		model.addActivity(new Activity("Idea 1",30,0,""),0);
-		model.addActivity(new Activity("Working in groups",35,1,""),0);
-		model.addActivity(new Activity("Idea 1 discussion",15,2,""),0);
-		
+
 		console.log("Activity name: " + model.parkedActivities.length);
-		console.log("Day Start: " + model.days[0].getStart());
 		var length = model.days.length;
 
 	});
