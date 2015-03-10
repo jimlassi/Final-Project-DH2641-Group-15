176c2c8e893903581fe6b5650babe4411675f6a8
miljonhunden <jian0415@gmail.com>
2015-03-10 15:06:46 +0100

    fixez

--- js/view/PlanningViewController.js
+++ js/view/PlanningViewController.js
@@ -5,34 +5,18 @@ var PlanningViewController = function(view, model ) {
 		$("#popupAddActivity").show();
 			
 
-/*			alert("activity box");	
-		
+	});
 
-		
-		function check_empty() {
-			if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-				alert("Fill All Fields !");
-			} 
-
-			else {
-				document.getElementById('form').submit();
-					alert("Form Submitted Successfully...");
-			}
-			}
-
-	*/});
+	//Confirm activity and hide popup
 	$("#confirm-activity").click(function(){
 			$("#popupAddActivity").hide();
 		});
-
-		var activityName = prompt("activity name");
-		var description = prompt("description");
 		
-	});
+	}
 
 
 
 
 
 
-}
+
