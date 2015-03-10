c08baeb775f328d98da9891104d101c905173f93
miljonhunden <jian0415@gmail.com>
2015-03-10 11:50:24 +0100

    changes

--- js/view/StartView.js
+++ js/view/StartView.js
@@ -1,7 +1,7 @@
 //ExampleView Object constructor
 var StartView = function (container, model) {
 	
-	
+	//håkan
 	model.addObserver(this);
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
