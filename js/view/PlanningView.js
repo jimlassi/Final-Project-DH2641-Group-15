0bf7e1e9ec5dc2c56571be644c8db571ecdb35f7
miljonhunden <jian0415@gmail.com>
2015-03-10 11:59:14 +0100

    added PlanningView.js

---
+++ js/view/PlanningView.js
@@ -0,0 +1,12 @@
+var PlanningView = function (container, model) {
+
+	model.addObserver(this);
+	this.PlanningView = container.find("#planning-view");
+	this.update = function(arg){
+		
+		//dosomething();
+	}
+
+
+
+}
