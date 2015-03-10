184b1facb9694a8a477099c3435d11478bc37d2c
tompa119 <anderssontomase@gmail.com>
2015-03-10 12:26:16 +0100

    Updated model.js, small fixes with layout

--- js/model/model.js
+++ js/model/model.js
@@ -1,147 +1,248 @@
-//DinnerModel Object constructor
-var DinnerModel = function() {
- 
+// JavaScript Document
+
+// The possible activity types
+var ActivityType = ["Presentation","Group Work","Discussion","Break"]
+
+// This is an activity constructor
+// When you want to create a new activity you just call
+// var act = new Activity("some activity",20,1,"Some description);
+function Activity(name,length,typeid,description){
+	var _name = name;
+	var _length = length;
+	var _typeid = typeid;
+	var _description = description;
 	
-	
-	
-	this.setAgendaItem = function(string){
-
-
-
-
+	// sets the name of the activity
+	this.setName = function(name) {
+		_name = name;
+		model.notifyObservers();
 	}
 
-	this.addObserver = function(observer) {
-		
-		observers.push(observer);
-		
-		}
-		
-		
-	var notifyObservers = function(obj) {
-		for(var i=0; i<observers.length; i++) 
-		{
-			observers[i].update(obj);
-		}	
-			
+	// get the name of the activity
+	this.getName = function(name) {
+		return _name;
+	}
+	
+	// sets the length of the activity
+	this.setLength = function(length) {
+		_length = length;
+		model.notifyObservers();
+	}
+
+	// get the name of the activity
+	this.getLength = function() {
+		return _length;
+	}
+	
+	// sets the typeid of the activity
+	this.setTypeId = function(typeid) {
+		_typeid = typeid;
+		model.notifyObservers();
+	}
+
+	// get the type id of the activity
+	this.getTypeId = function() {
+		return _typeid;
+	}
+	
+	// sets the description of the activity
+	this.setDescription = function(description) {
+		_description = description;
+		model.notifyObservers();
+	}
+
+	// get the description of the activity
+	this.getDescription = function() {
+		return _description;
+	}
+	
+	// This method returns the string representation of the
+	// activity type.
+	this.getType = function () {
+		return ActivityType[_typeid];
+	};
+}
+
+// This is a day consturctor. You can use it to create days, 
+// but there is also a specific function in the Model that adds
+// days to the model, so you don't need call this yourself.
+function Day(startH,startM) {
+	this._start = startH * 60 + startM;
+	this._activities = [];
+
+	// sets the start time to new value
+	this.setStart = function(startH,startM) {
+		this._start = startH * 60 + startM;
+		model.notifyObservers();
+	}
+
+	// returns the total length of the acitivities in 
+	// a day in minutes
+	this.getTotalLength = function () {
+		var totalLength = 0;
+		$.each(this._activities,function(index,activity){
+			totalLength += activity.getLength();
+		});
+		return totalLength;
+	};
+	
+	// returns the string representation Hours:Minutes of 
+	// the end time of the day
+	this.getEnd = function() {
+		var end = this._start + this.getTotalLength();
+		return Math.floor(end/60) + ":" + end % 60;
+	};
+	
+	// returns the string representation Hours:Minutes of 
+	// the start time of the day
+	this.getStart = function() {
+		return Math.floor(this._start/60) + ":" + this._start % 60;
+	};
+	
+	// returns the length (in minutes) of activities of certain type
+	this.getLengthByType = function (typeid) {
+		var length = 0;
+		$.each(this._activities,function(index,activity){
+			if(activity.getTypeId() == typeid){
+				length += activity.getLength();
 			}
-
-	this.setFilter = function(f){
-		
-		filter = f;
-		notifyObservers();
-	}
-	this.getFilter = function(){
-		
-		return filter 
-		}
-		
-		
-	this.setNumberOfHours = function(num) {
-		if(num>0) {
-			guests = num;
-			notifyObservers();
-		}
-	}
-
-	// should return 
-	this.getNumberOfHours= function() {
-		return guests;
-	}
+		});
+		return length;
+	};
 	
+	// adds an activity to specific position
+	// if the position is not provided then it will add it to the 
+	// end of the list
+	this._addActivity = function(activity,position){
+		if(position != null){
+			this._activities.splice(position,0,activity);
+		} else {
+			this._activities.push(activity);
+		}
+	};
 	
-	this.getType = function() {
-		return type;
-	}
-
-	this.setType = function(t) {
-		
-		type = t;
-		notifyObservers();
-	}
+	// removes an activity from specific position
+	// this method will be called when needed from the model
+	// don't call it directly
+	this._removeActivity = function(position) {
+		return this._activities.splice(position,1)[0];
+	};
 	
-	this.setTempID = function(id){
-		
-		tempId = id;
-		notifyObservers();
-		
-	}
-	this.getTempID = function(){
-		
-		return tempId;
-	}
-	//Returns the dish that is on the menu for selected type 
-	this.getSelectedDish = function(type) {
-		return menu[type];
-	}
-
-	//Returns all the dishes on the menu.
-	this.getFullMenu = function() {
-		var fullMenu = [];
-		for(key in menu) {
-			fullMenu.push(this.getDish(menu[key]));
+	// moves activity inside one day
+	// this method will be called when needed from the model
+	// don't call it directly
+	this._moveActivity = function(oldposition,newposition) {
+		// In case new position is greater than the old position and we are not moving
+		// to the last position of the array
+		if(newposition > oldposition && newposition < this._activities.length - 1) {
+			newposition--;
 		}
-		return fullMenu;
-	}
+		var activity = this._removeActivity(oldposition);
+		this._addActivity(activity, newposition);
+	};
+}
 
-	//Returns all ingredients for all the dishes on the menu.
-	this.getAllIngredients = function() {
-		var ingredients = [];
-		for(key in menu) {
-			var dish = this.getDish(menu[key]);
-			ingredients = ingredients.concat(dish.ingredients);
+
+// this is our main module that contians days and praked activites
+function Model(){
+	this.days = [];
+	this.parkedActivities = [];
+	
+	// adds a new day. if startH and startM (start hours and minutes)
+	// are not provided it will set the default start of the day to 08:00
+	this.addDay = function (startH,startM) {
+		var day;
+		if(startH){
+			day = new Day(startH,startM);
+		} else {
+			day = new Day(8,0);
 		}
-		return ingredients;
-	}
-
-	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
-	this.getTotalMenuPrice = function() {
-		var ingredients = this.getAllIngredients();
-		var sum = 0.;
-		for(key in ingredients) {
-			sum += parseFloat(ingredients[key].price) * this.getNumberOfGuests();
-		}
-		return sum;
-	}
-
-	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
-	//it is removed from the menu and the new one added.
-	this.addDishToMenu = function(id) {
-		menu[this.getDish(id).type] = id; 
-		
-		notifyObservers();
-	}
-
-	//Removes dish from menu
-	this.removeDishFromMenu = function(id) {
-		var type = this.getDish(id).type;
-		if(menu[type] == id) {
-			delete menu[type];
-		}
-		notifyObservers();
-	}
-
-	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
-	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
-	//if you don't pass any filter all the dishes will be returned
-	this.getAllDishes = function (type,filter) {
-	  return $(dishes).filter(function(index,dish) {
-		var found = true;
-		if(filter){
-			found = false;
-			$.each(dish.ingredients,function(index,ingredient) {
-				if(ingredient.name.indexOf(filter)!=-1) {
-					found = true;
-				}
-			});
-			if(dish.name.indexOf(filter) != -1)
-			{
-				found = true;
+		this.days.push(day);
+		this.notifyObservers();
+		return day;
+	};
+	
+	// add an activity to model
+	this.addActivity = function (activity,day,position) {
+		if(day != null) {
+			this.days[day]._addActivity(activity,position);
+		} else {
+			if (position != null) {
+				this.parkedActivities.splice(position,0,activity);
 			}
+			else this.parkedActivities.push(activity);
 		}
-	  	return dish.type == type && found;
-	  });	
+		this.notifyObservers();
 	}
+	
+	// add an activity to parked activities
+	this.addParkedActivity = function(activity,position){
+		this.addActivity(activity,null,position);
+	};
+	
+	// remove an activity on provided position from parked activites 
+	this.removeParkedActivity = function(position) {
+		act = this.parkedActivities.splice(position,1)[0];
+		this.notifyObservers();
+		return act;
+	};
+	
+	// moves activity between the days, or day and parked activities.
+	// to park activity you need to set the new day to null
+	// to move a parked activity to let's say day 0 you set oldday to null
+	// and new day to 0
+	this.moveActivity = function(oldday, oldposition, newday, newposition) {
+		if(oldday !== null && oldday == newday) {
+			this.days[oldday]._moveActivity(oldposition,newposition);
+		}else if(oldday == null && newday == null) {
+			var activity = this.removeParkedActivity(oldposition);
+			this.addParkedActivity(activity,newposition);
+		}else if(oldday == null) {
+			var activity = this.removeParkedActivity(oldposition);
+			this.days[newday]._addActivity(activity,newposition);
+		}else if(newday == null) {
+			var activity = this.days[oldday]._removeActivity(oldposition);
+			this.addParkedActivity(activity,newposition);
+		} else {
+			var activity = this.days[oldday]._removeActivity(oldposition);
+			this.days[newday]._addActivity(activity,newposition);
+		}
+		this.notifyObservers();
+	};
+	
+	//*** OBSERVABLE PATTERN ***
+	var listeners = [];
+	
+	this.notifyObservers = function (args) {
+	    for (var i = 0; i < listeners.length; i++){
+	        listeners[i].update(args);
+	    }
+	};
+	
+	this.addObserver = function (listener) {
+	    listeners.push(listener);
+	};
+	//*** END OBSERVABLE PATTERN ***
+}
+
+// this is the instance of our main model
+// this is what you should use in your application
+var model = new Model();
 
 
+// you can use this method to create some test data and test your implementation
+function createTestData(){
+	model.addDay();
+	model.addActivity(new Activity("Introduction",10,0,""),0);
+	model.addActivity(new Activity("Idea 1",30,0,""),0);
+	model.addActivity(new Activity("Working in groups",35,1,""),0);
+	model.addActivity(new Activity("Idea 1 discussion",15,2,""),0);
+	model.addActivity(new Activity("Coffee break",20,3,""),0);
+	
+	console.log("Day Start: " + model.days[0].getStart());
+	console.log("Day End: " + model.days[0].getEnd());
+	console.log("Day Length: " + model.days[0].getTotalLength() + " min");
+	$.each(ActivityType,function(index,type){
+		console.log("Day '" + ActivityType[index] + "' Length: " +  model.days[0].getLengthByType(index) + " min");
+	});
 }
