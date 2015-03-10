11b6a75be09318499fc0b2ad3e1b652409ab935a
miljonhunden <jian0415@gmail.com>
2015-03-10 16:25:29 +0100

    Updated add activity pop-up

--- js/view/PlanningViewController.js
+++ js/view/PlanningViewController.js
@@ -1,5 +1,6 @@
 var PlanningViewController = function(view, model ) {
 	
+	var x =1;
 	//Function To Display Popup		
 	$("#add-activity").click(function(){
 		$("#popupAddActivity").show();
@@ -9,7 +10,27 @@ var PlanningViewController = function(view, model ) {
 
 	//Confirm activity and hide popup
 	$("#confirm-activity").click(function(){
-			$("#popupAddActivity").hide();
+		
+				if (document.getElementById('name').value == "" || document.getElementById('description').value == "") {
+					alert("Fill All Fields !");
+					
+				} 
+				else if (isNaN(parseInt(document.getElementById('length').value)))   {
+					alert("Must be a number");
+				}
+				else {
+					//document.getElementById('form').submit();
+					model.addActivity(new Activity(document.getElementById('name').value, document.getElementById('length').value, document.getElementById('type').value, document.getElementById('description').value));
+					console.log(model.parkedActivities[0].getName());
+					console.log(model.parkedActivities[0].getLength());
+					console.log(model.parkedActivities[0].getTypeId());
+					console.log(model.parkedActivities[0].getDescription());
+					$("#popupAddActivity").hide();
+					$("#start-view").hide();
+					$("#planning-view").show();
+			}
+
+			
 		});
 		
 	}
