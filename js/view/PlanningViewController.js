ba54c8d1b820868723ce61977075e1cd0d54c0af
miljonhunden <jian0415@gmail.com>
2015-03-10 12:03:45 +0100

    added PlanningViewController.js

---
+++ js/view/PlanningViewController.js
@@ -0,0 +1,13 @@
+var PlanningViewController = function(view, model ) {
+	
+			
+	//$("#start-button").click(function(){
+		//$("#start-view").hide();
+		//$("#planning-view").show();
+	//});
+
+
+
+
+
+}
