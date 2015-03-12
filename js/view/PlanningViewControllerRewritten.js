8963a74f7816883d9d9ddfaff695c33a4ab59bf2
Gustav Hammarlund <hammarlund.gustav@gmail.com>
2015-03-12 02:53:14 +0100

    laborating with different drag and drop approach - modifications in js/view/*Rewritten.js and testinho/*

---
+++ js/view/PlanningViewControllerRewritten.js
@@ -0,0 +1,74 @@
+var PlanningViewControllerRewritten = function(view, model ) {
+	
+	//Function To Display Popup		
+	$("#add-activity").click(function(){
+		$("#popupAddActivity").show();
+			
+
+	});
+
+	//Confirm activity and hide popup
+	$("#confirm-activity").click(function(){
+		
+				if (document.getElementById('name').value == "" || document.getElementById('description').value == "") {
+					alert("Fill All Fields !");
+					
+				} 
+				else if (isNaN(parseInt(document.getElementById('length').value)))   {
+					alert("Must be a number");
+				}
+				else {
+					//document.getElementById('form').submit();
+					model.addActivity(new Activity(document.getElementById('name').value, document.getElementById('length').value, document.getElementById('type').value, document.getElementById('description').value));
+					/*tezt
+					console.log(model.parkedActivities[0].getName());
+					console.log(model.parkedActivities[0].getLength());
+					console.log(model.parkedActivities[0].getTypeId());
+					console.log(model.parkedActivities[0].getDescription());
+					*/
+					$("#popupAddActivity").hide();
+					document.getElementById('name').value ="";
+					document.getElementById('length').value="";
+					document.getElementById('type').value="0"; 
+					document.getElementById('description').value="";
+			}
+					
+			
+		});
+	//Exit add activity to hide popup
+	$("#exit-activity").click(function(){
+				$("#popupAddActivity").hide();
+	
+	});
+	
+	// Add day button
+	$("#add-day").click(function() {
+		view.days++
+	});
+	
+	
+	
+	
+	$(function() {
+    $( ".row" ).draggable();
+    $( "#day-rect" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "div" )
+            .html( "Dropped!" );
+      }
+    });
+  });
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
---
+++ js/view/PlanningViewRewritten.js
@@ -0,0 +1,116 @@
+var PlanningViewRewritten = function (container, model) {
+
+	model.addObserver(this);
+	this.PlanningView = container.find("#planning-view");
+	this.addActivity = container.find("#add-day");
+
+	
+
+	var name = "";
+	var time; 
+	var description; 
+	var type;
+	var days = 2;
+
+	
+	function getTable(){
+		this.table = container.find("#activity-table-content");
+		this.table.html("");
+		
+		// creating the ul element to contain the activities
+		var table = document.createElement("ul");
+		table.addClass = "parked-table";
+		
+		
+		// looping through the activities and creating list entries for each
+		for (i=0; i<model.parkedActivities.length; i++) {
+			var row = document.createElement("li");
+			row.setAttribute("id", "activity-row");
+			row.className = "row";
+			
+			// creating containers for the activity information
+			/*var timeContainer = document.createElement("p");
+			time = model.parkedActivities[i].getLength();
+			timeContainer.appendChild(document.createTextNode(time));
+			row.appendChild(timeContainer);*/
+			
+			var nameContainer = document.createElement("p");
+			name = model.parkedActivities[i].getName();
+			nameContainer.appendChild(document.createTextNode(name));
+			row.appendChild(nameContainer);
+			
+			
+			time = model.parkedActivities[i].getLength();
+			description = model.parkedActivities[i].getDescription();
+			
+			table.appendChild(row);
+			
+		}
+		document.getElementById("activity-table-content").appendChild(table);
+	}
+
+	
+	function createDays () {
+		
+		this.daySurround = container.find("#days");
+		daySurround.html("");
+		
+		for (i=0; i < days ; i++) {
+			var tmpDay = document.createElement("div");
+			//tempId = "day-container";
+			tmpDay.setAttribute("id", "day-container"); 
+			tmpDay.className = "day-rect";
+			
+			document.getElementById("days").appendChild(tmpDay);
+		}
+	}
+
+
+	this.update = function(arg){
+		
+	
+		getTable();
+		createDays();
+		
+		
+		$(function() {
+
+			// let the parked items be draggable
+			var $parked = $("#activity-table-content");
+			
+			$( "div", $parked ).draggable({
+				//cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+				revert: "invalid", // when not dropped, the item will revert back to its initial position
+				containment: "document",
+				helper: "original",
+				cursor: "move"
+			});
+			
+			// let the parked items be droppable
+			$("#activity-table-content").droppable({
+				activeClass: "ui-state-highlight",
+				drop: function( event, ui ) {
+				// remove activity from current day
+				}
+			});
+			
+			
+			
+			// Let day be droppable
+			var $day = $("#day-container");
+			
+			$day.droppable({
+
+			drop: function( event, ui ) {
+				// add activity to day in the model
+				}
+			});
+
+
+			 
+			 
+		});
+
+
+	}
+}
